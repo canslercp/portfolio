b60419004a23a6997e2ebaa7a8af3b6e06dc0e2b
canslercp <canslercp@gmail.com>
2022-08-23 17:17:44 -0400

    added stying to projects and contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 export default function Contact() {
     return(
-        <div>
-            <p>Email: canslercp@gmail.com   Phone: 828-320-6993</p>
-            <p>GitHub: github.com/canslercp   LinkedIn: linkedin.com/in/corbin-cansler/</p>
+        <div className='contact-container'>
+            <div className='contact'>
+            <p>Email: canslercp@gmail.com</p>  
+            <p>Phone: 828-320-6993</p>
+            </div>
+            <div className='contact'>
+            <a href='https://github.com/canslercp'>
+            <p>GitHub: github.com/canslercp</p>  
+            </a>
+            <a href='https://linkedin.com/in/corbin-cansler/'>
+            <p>LinkedIn: linkedin.com/in/corbin-cansler/</p>
+            </a>
+            </div>
             <p>placeholder for resume google doc</p>
         </div>
     )
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,15 +5,17 @@ import weather from '../../assets/weather-app.png'
 
 export default function Projects() {
     return (
-        <div>
-            <a href='https://rerun-game.herokuapp.com/'>
-            <img src={rerun} alt='A text-based interactive fiction game where you must beat the clock, or Rerun!'/>
-            </a>
+        
+        <div class='projects'>
+            
             <a href='https://canslercp.github.io/the_final_countdown/'>
-            <img src={finalCountdown} alt='A front-end, doomsday tracker where you click through and save recent disaster data'/>
+            <img className='project-image' src={finalCountdown} alt='A front-end, doomsday tracker where you click through and save recent disaster data'/>
+            </a>
+            <a href='https://rerun-game.herokuapp.com/'>
+            <img className='project-image' src={rerun} alt='A text-based interactive fiction game where you must beat the clock, or Rerun!'/>
             </a>
             <a href='https://canslercp.github.io/weather-dashboard-corbin-cansler/'>
-            <img src={weather} alt='A  weather dashboard that will run in the browser and use the OpenWeather One Call API to retrieve weather data for cities.'/>
+            <img className='project-image' src={weather} alt='A  weather dashboard that will run in the browser and use the OpenWeather One Call API to retrieve weather data for cities.'/>
             </a>
         </div>
     );
