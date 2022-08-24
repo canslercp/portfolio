d5131ebe884746f94653e79e8f14d53b8b77a653
canslercp <canslercp@gmail.com>
2022-08-23 21:40:36 -0400

    fixed menu icon bug

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,25 +3,30 @@ import React from 'react';
 // Useing object destructuring to pluck off variables from the props object and assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
     return (
-        <nav className="navbar navbar-expand-lg nav-custom">
+        <nav className="navbar navbar-expand-lg fixed-top nav-custom">
             <div className="container-fluid">
                 {/* link to home page */}
                 <a className="navbar-brand name-title"
                     href="#home"
                     onClick={() => handlePageChange('Home')}
                 >CORBIN + CANSLER</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon "></span>
                 </button>
 
-                <div className="collapse navbar-collapse navTabs" id="navbarNav">
-                    <ul className="navbar-nav">
+                <div className="offcanvas offcanvas-end navTabs" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        {/* <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navigate</h5> */}
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <ul className="navbar-nav offcanvas-body">
                         <li className="nav-item">
                             <a
                                 href="#about"
                                 onClick={() => handlePageChange('About')}
                                 className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
                                 aria-current={currentPage === 'About' ? 'page' : null}
+                                data-bs-dismiss="offcanvas"
                             >About</a>
                         </li>
                         <li className="nav-item">
@@ -30,6 +35,7 @@ function NavTabs({ currentPage, handlePageChange }) {
                                 onClick={() => handlePageChange('Projects')}
                                 className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
                                 aria-current={currentPage === 'Projects' ? 'page' : null}
+                                data-bs-dismiss="offcanvas"
                             >Projects</a>
                         </li>
                         <li className="nav-item">
@@ -38,6 +44,7 @@ function NavTabs({ currentPage, handlePageChange }) {
                                 onClick={() => handlePageChange('Contact')}
                                 className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                                 aria-current={currentPage === 'Projects' ? 'page' : null}
+                                data-bs-dismiss="offcanvas"
                             >Contact</a>
                         </li>
                     </ul>
