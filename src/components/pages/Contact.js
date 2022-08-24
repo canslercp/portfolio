a7c1abadbc333cb0c1cab3327134b8fe577959a1
canslercp <canslercp@gmail.com>
2022-08-24 10:27:23 -0400

    updated links to open in new browser

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,14 +8,14 @@ export default function Contact() {
             <p>Phone: 828-320-6993</p>
             </div>
             <div className='contact'>
-            <a href='https://github.com/canslercp'>
+            <a href='https://github.com/canslercp' target="_blank" rel="noreferrer">
             <p>GitHub: github.com/canslercp</p>  
             </a>
-            <a href='https://linkedin.com/in/corbin-cansler/'>
+            <a href='https://linkedin.com/in/corbin-cansler/' target="_blank" rel="noreferrer">
             <p>LinkedIn: linkedin.com/in/corbin-cansler/</p>
             </a>
             </div>
-            <a href='https://docs.google.com/document/d/10mrgqejtw0zkWFWDRSsowHf5gC-NDGzWleriML5xBJ0/edit'>
+            <a href='https://docs.google.com/document/d/10mrgqejtw0zkWFWDRSsowHf5gC-NDGzWleriML5xBJ0/edit' target="_blank" rel="noreferrer">
             <p>View and download resume</p>
             </a>
         </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -8,13 +8,13 @@ export default function Projects() {
         
         <div class='projects'>
             
-            <a href='https://canslercp.github.io/the_final_countdown/'>
+            <a href='https://canslercp.github.io/the_final_countdown/' target="_blank" rel="noreferrer">
             <img className='project-image' src={finalCountdown} alt='A front-end, doomsday tracker where you click through and save recent disaster data'/>
             </a>
-            <a href='https://rerun-game.herokuapp.com/'>
+            <a href='https://rerun-game.herokuapp.com/' target="_blank" rel="noreferrer">
             <img className='project-image' src={rerun} alt='A text-based interactive fiction game where you must beat the clock, or Rerun!'/>
             </a>
-            <a href='https://canslercp.github.io/weather-dashboard-corbin-cansler/'>
+            <a href='https://canslercp.github.io/weather-dashboard-corbin-cansler/' target="_blank" rel="noreferrer">
             <img className='project-image' src={weather} alt='A  weather dashboard that will run in the browser and use the OpenWeather One Call API to retrieve weather data for cities.'/>
             </a>
         </div>
