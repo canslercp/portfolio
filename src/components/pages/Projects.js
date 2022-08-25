5d74e7ecbce49f27107c54490befc430aeba49d0
canslercp <canslercp@gmail.com>
2022-08-25 16:50:19 -0400

    added descriptions to projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,17 +6,25 @@ import weather from '../../assets/weather-app.png'
 export default function Projects() {
     return (
         
-        <div class='projects'>
-            
+        <div className='project-container'>
+            <div className='project'>
             <a href='https://canslercp.github.io/the_final_countdown/' target="_blank" rel="noreferrer">
             <img className='project-image' src={finalCountdown} alt='A front-end, doomsday tracker where you click through and save recent disaster data'/>
             </a>
+            <p>Front-end doomsday tracker where you click through and save recent disaster data</p>
+            </div>
+            <div className='project'>
             <a href='https://rerun-game.herokuapp.com/' target="_blank" rel="noreferrer">
             <img className='project-image' src={rerun} alt='A text-based interactive fiction game where you must beat the clock, or Rerun!'/>
             </a>
+            <p>A Full-Stack, Text-based, speed-run adventure game where you play to beat the clock</p>
+            </div>
+            <div className='project'>
             <a href='https://canslercp.github.io/weather-dashboard-corbin-cansler/' target="_blank" rel="noreferrer">
             <img className='project-image' src={weather} alt='A  weather dashboard that will run in the browser and use the OpenWeather One Call API to retrieve weather data for cities.'/>
             </a>
-        </div>
+            <p>A weather dashboard that runs in the browser and uses the OpenWeather One Call API to retrieve weather data for cities.</p>
+            </div >
+        </div >
     );
 }
